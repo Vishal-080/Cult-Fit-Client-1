305a6943ea5cabc710d6be5c19350b9062577a96
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-12-14 09:52:36 +0530

    navbar icons added

---
+++ cultfit/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+
+
+export const Navbar = () => {
+    return <div>
+        <div>
+            <img src="/icons/Cultfitlogo.svg" alt=""/>
+        </div>
+        <div>
+            <div>
+                <img src="/icons/runicon.svg" alt=""/>
+                <p>CULT</p>
+            </div>
+            <div>
+                <img src="/icons/Video.svg" alt=""/>
+                <p>LIVE</p>
+            </div>
+            <div>
+                <img src="/icons/eaticon.svg" alt=""/>
+                <p>EAT</p>
+            </div>
+            <div>
+                <img src="/icons/Mind.svg" alt=""/>
+                <p>MIND</p>
+            </div>
+            <div>
+                <img src="/icons/care.svg" alt=""/>
+                <p>CARE</p>
+            </div>
+            <div>
+                <img src="/icons/Bag.svg" alt=""/>
+                <p>STORE</p>
+            </div>
+
+        </div>
+        <div>
+            <div>
+                <img src="/icons/Ellipse.svg" alt=""/>
+                <p>Location</p>
+            </div>
+            <div>
+                <img src="/icons/usericon.svg" alt=""/>
+                <p>Login</p>
+            </div>
+            <div>
+                <img src="/icons/shoppingcarticon.svg" alt=""/>
+            </div>
+        </div>
+    </div>
+}
