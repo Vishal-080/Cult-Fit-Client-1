01c18bcdb968945457046cb42b8f87d050a32c98
Vishal Rathod <vrathod591@gmail.com>
2021-12-16 00:05:11 +0530

    Footer Done

---
+++ cultfit/src/components/cult-LowerHalf/Footer_2/Footer2.jsx
@@ -0,0 +1,108 @@
+import "./footer2.css";
+
+export const Footer2 = () => {
+  return (
+    <div class="container">
+      <div class="related_searches_section">
+        <h4>Related searches</h4>
+        <div class="search_list">
+          <div>
+            <div>Gym Near Me</div>
+            <div>Yoga for Back Pain</div>
+            <div>Chest Workouts for Men</div>
+            <div>Weight Loss Workout</div>
+            <div>Yoga Poses for Beginners</div>
+            <div>Surya Namaskar for Beginners</div>
+            <div>Gym in Chembur</div>
+            <div>Gym in Vasant Kunj</div>
+            <div>Gym in Juhu</div>
+            <div>Gym in Mumbai</div>
+            <div>Gym in Kochi</div>
+          </div>
+          {/***** section 1 *****/}
+          <div>
+            <div>Gym Near Me</div>
+            <div>Yoga for Back Pain</div>
+            <div>Chest Workouts for Men</div>
+            <div>Weight Loss Workout</div>
+            <div>Yoga Poses for Beginners</div>
+            <div>Surya Namaskar for Beginners</div>
+            <div>Gym in Chembur</div>
+            <div>Gym in Vasant Kunj</div>
+            <div>Gym in Juhu</div>
+            <div>Gym in Mumbai</div>
+            <div>Gym in Kochi</div>
+          </div>
+          {/***** section 2 *****/}
+          <div>
+            <div>Gym Near Me</div>
+            <div>Yoga for Back Pain</div>
+            <div>Chest Workouts for Men</div>
+            <div>Weight Loss Workout</div>
+            <div>Yoga Poses for Beginners</div>
+            <div>Surya Namaskar for Beginners</div>
+            <div>Gym in Chembur</div>
+            <div>Gym in Vasant Kunj</div>
+            <div>Gym in Juhu</div>
+            <div>Gym in Mumbai</div>
+            <div>Gym in Kochi</div>
+          </div>
+          {/***** section 3 *****/}
+          <div>
+            <div>Gym Near Me</div>
+            <div>Yoga for Back Pain</div>
+            <div>Chest Workouts for Men</div>
+            <div>Weight Loss Workout</div>
+            <div>Yoga Poses for Beginners</div>
+            <div>Surya Namaskar for Beginners</div>
+            <div>Gym in Chembur</div>
+            <div>Gym in Vasant Kunj</div>
+            <div>Gym in Juhu</div>
+            <div>Gym in Mumbai</div>
+            <div>Gym in Kochi</div>
+          </div>
+          {/***** section 4 *****/}
+          <div>
+            <div>Gym Near Me</div>
+            <div>Yoga for Back Pain</div>
+            <div>Chest Workouts for Men</div>
+            <div>Weight Loss Workout</div>
+            <div>Yoga Poses for Beginners</div>
+            <div>Surya Namaskar for Beginners</div>
+            <div>Gym in Chembur</div>
+            <div>Gym in Vasant Kunj</div>
+            <div>Gym in Juhu</div>
+            <div>Gym in Mumbai</div>
+            <div>Gym in Kochi</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="bottom">
+        <div class="links">
+          <div>Contact us</div>
+          <div>FAQ</div>
+          <div>{`Terms & conditions`}</div>
+          <div>Privacy policy</div>
+          <div>Refund policy</div>
+        </div>
+        <div class="social_icons">
+          <div>
+            <img src={"./FooterImages/facebook_icon_white.svg"} alt="" />
+          </div>
+          <div>
+            <img src={"./FooterImages/instagram_icon_white.svg"} alt="" />
+          </div>
+          <div>
+            <img src={"./FooterImages/twitter_icon_white.svg"} alt="" />
+          </div>
+          <div>
+            <img src={"./FooterImages/youtube_icon_white_1.svg"} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer2;
