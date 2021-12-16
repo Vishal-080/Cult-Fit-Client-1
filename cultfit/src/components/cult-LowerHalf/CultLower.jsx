f76c6e23fcf3245bc47ce3d301653e8ac22fdb97
Vishal Rathod <vrathod591@gmail.com>
2021-12-17 00:37:08 +0530

    Cult Lower Page added

---
+++ cultfit/src/components/cult-LowerHalf/CultLower.jsx
@@ -0,0 +1,19 @@
+import WorkoutGrid from "./WorkoutGrid";
+import QuickLinks from "./QuickLinks";
+import BeSpirited from "./BeSpirited";
+import FAQ from "./FAQ";
+// import Footer2 from "./Footer2";
+
+export const CultLower = () => {
+  return (
+    <>
+      <WorkoutGrid />
+      <QuickLinks />
+      <BeSpirited />
+      <FAQ />
+      {/* <Footer2 /> */}
+    </>
+  );
+};
+
+export default CultLower;
