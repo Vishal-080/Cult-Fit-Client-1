0b4da612ef121393f9f52a7d248b1fdb9135f6f9
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-12-16 13:46:25 +0530

    changes in workout.jsx

--- cultfit/src/Pages/Routes.jsx
+++ cultfit/src/Pages/Routes.jsx
@@ -1,6 +1,7 @@
 import { Switch, Route, Link } from 'react-router-dom'
 import { Cult } from '../components/cult/Cult'
 import { LandingPage } from '../components/Landing/Landing'
+import Workout from '../components/Workout/Workout'
 
 
 export const Allroutes = () => {
@@ -12,6 +13,10 @@ export const Allroutes = () => {
 
         <Route exact path="/cult">
             <Cult/>
+        </Route>
+
+        <Route exact path="/sessiontype/:session">
+            <Workout/>
         </Route>
 
         <Route>
