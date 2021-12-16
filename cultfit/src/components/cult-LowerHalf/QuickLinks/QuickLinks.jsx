bfcfd591a85cf33eb7992d373e9d1c1512c8ffe0
Vishal Rathod <vrathod591@gmail.com>
2021-12-17 00:40:35 +0530

    Quick Links CSS Updated

--- cultfit/src/components/cult-LowerHalf/QuickLinks/QuickLinks.jsx
+++ cultfit/src/components/cult-LowerHalf/QuickLinks/QuickLinks.jsx
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 const QuickLink = styled.div`
   width: 1200px;
-  margin: auto;
+  margin: 93px auto;
+  height: 140px;
+  text-align: left;
   /* border: 1px solid red; */
-  height: 185px;
 `;
 const QuickTitle = styled.div`
   color: #000;
@@ -14,23 +15,22 @@ const QuickTitle = styled.div`
   padding: 12px;
   align-content: center;
   margin-bottom: -6px;
+  border-bottom: 0.5px solid #a2a2a2;
 `;
 
 const Links = styled.div`
   font-weight: bold;
   font-size: 18px;
   padding: 12px;
+  border-bottom: 0.5px solid #a2a2a2;
 `;
 
 export const QuickLinks = () => {
   return (
     <QuickLink>
       <QuickTitle>Quick Links</QuickTitle>
-      <hr />
       <Links>Help & Support</Links>
-      <hr />
       <Links>Terms and Conditions</Links>
-      <hr />
     </QuickLink>
   );
 };
